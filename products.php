9d8c0ec994cb310e1153244acac778d994ca25a5
martinfryc1 <martin.fryc@roihunter.com>
2019-09-04 09:32:05 +0200

    hotfix - try catch with cause for all HTTP requests [GSTV-649]

--- products.php
+++ products.php
@@ -24,64 +24,57 @@ const RH_PRODUCT_PER_PAGE = 100;
 
 ROIHunterAuthenticator::getInstance()->authenticate();
 
-$roihunterModule = Roihunter::getModuleInstance();
+try {
+    $roihunterModule = Roihunter::getModuleInstance();
 
-$id_shop = $roihunterModule->getShopFromUrl($_SERVER['HTTP_HOST']);
-Context::getContext()->shop->id = $id_shop;
+    $id_shop = $roihunterModule->getShopFromUrl($_SERVER['HTTP_HOST']);
+    Context::getContext()->shop->id = $id_shop;
 
-
-/*
-$stream = file_get_contents('php://input');
-
-$data = json_decode($stream, true);
-
-
-// TODO ... page from data, id_lang from data
-
-
-$page = (int)$data['page']?(int)$data['page']:1;
-
-$id_lang = (int)$data['id_lang']?(int)$data['id_lang']:(int)Configuration::get('PS_LANG_DEFAULT', null, null, $id_shop);
-*/
-$id_lang = (Tools::getIsset(Tools::getValue('id_lang')) && (int) Tools::getValue('id_lang')) ?
-    (int) Tools::getValue('id_lang'):
-    (int) Configuration::get(
-        'PS_LANG_DEFAULT',
-        null,
-        null,
-        $id_shop
-    );
+    $id_lang = (Tools::getIsset(Tools::getValue('id_lang')) && (int)Tools::getValue('id_lang')) ?
+        (int)Tools::getValue('id_lang') :
+        (int)Configuration::get(
+            'PS_LANG_DEFAULT',
+            null,
+            null,
+            $id_shop
+        );
 
 
-$page = Tools::getValue('page');
-if (is_null($page) || empty($page)) {
-    $page = RH_FIRST_PRODUCT_PAGE;
-} else {
-    $page = (int) $page;
-}
-if (!is_numeric($page) || $page < RH_FIRST_PRODUCT_PAGE) {
-    header("HTTP/1.1 400 Bad Request");
-    echo "Page parameter is not valid.";
-    die();
-}
+    $page = Tools::getValue('page');
+    if (is_null($page) || empty($page)) {
+        $page = RH_FIRST_PRODUCT_PAGE;
+    } else {
+        $page = (int)$page;
+    }
+    if (!is_numeric($page) || $page < RH_FIRST_PRODUCT_PAGE) {
+        header("HTTP/1.1 400 Bad Request");
+        echo "Page parameter is not valid.";
+        die();
+    }
 
-$offset = RH_PRODUCT_PER_PAGE * ($page - RH_FIRST_PRODUCT_PAGE);
+    $offset = RH_PRODUCT_PER_PAGE * ($page - RH_FIRST_PRODUCT_PAGE);
 
-$sql = 'SELECT s.id_product, pa.id_product_attribute FROM
+    $sql = 'SELECT s.id_product, pa.id_product_attribute FROM
     ' . _DB_PREFIX_ . 'product_shop s LEFT JOIN ' . _DB_PREFIX_ . 'product_attribute pa
-    ON s.id_product = pa.id_product AND s.id_shop = ' . (int) $id_shop . ' WHERE
+    ON s.id_product = pa.id_product AND s.id_shop = ' . (int)$id_shop . ' WHERE
     s.active = 1 
     ORDER BY s.id_product, pa.id_product_attribute 
     LIMIT ' . RH_PRODUCT_PER_PAGE . ' OFFSET ' . $offset;
-$items = Db::getInstance()->executeS($sql);
+    $items = Db::getInstance()->executeS($sql);
 
-$json = new ProductJson($roihunterModule->getImageType());
-$jsonData = [];
-foreach ($items as $item) {
-    $jsonData[] = $json->getJson($item['id_product'], $item['id_product_attribute'], $id_lang, $id_shop);
+    $json = new ProductJson($roihunterModule->getImageType());
+    $jsonData = [];
+    foreach ($items as $item) {
+        $jsonData[] = $json->getJson($item['id_product'], $item['id_product_attribute'], $id_lang, $id_shop);
+    }
+
+    header("HTTP/1.1 200 OK");
+    header("Content-Type:application/json");
+    echo json_encode($jsonData);
+    die();
+
+} catch (Exception $e) {
+    header("HTTP/1.1 500 Internal Server Error", true, 500);
+    echo("Unknown server exception: " . $e->getMessage() . " \nStacktrace: " . $e->getTraceAsString());
+    die();
 }
-
-header("HTTP/1.1 200 OK");
-header("Content-Type:application/json");
-echo json_encode($jsonData);
-die();
--- state.php
+++ state.php
@@ -21,31 +21,37 @@ require_once(_PS_MODULE_DIR_ . 'roihunter/roihunter.php');
 
 ROIHunterAuthenticator::getInstance()->authenticate();
 
-$roihunterModule = Roihunter::getModuleInstance();
+try {
+    $roihunterModule = Roihunter::getModuleInstance();
 
-$id_shop = $roihunterModule->getShopFromUrl($_SERVER['HTTP_HOST']);
-Context::getContext()->shop->id = $id_shop;
+    $id_shop = $roihunterModule->getShopFromUrl($_SERVER['HTTP_HOST']);
+    Context::getContext()->shop->id = $id_shop;
 
-if ($_SERVER['REQUEST_METHOD'] == 'PUT') {
-    $stream = Tools::file_get_contents('php://input');
-    $data = json_decode($stream, true);
+    if ($_SERVER['REQUEST_METHOD'] == 'PUT') {
+        $stream = Tools::file_get_contents('php://input');
+        $data = json_decode($stream, true);
 
-    if ($data[ROIHunterStorage::RH_SYSTEM_USER_ID] != null && !is_int($data[ROIHunterStorage::RH_SYSTEM_USER_ID])) {
-        header('HTTP/1.0 400 Bad Request - id is not int.', true, 400);
+        if ($data[ROIHunterStorage::RH_SYSTEM_USER_ID] != null && !is_int($data[ROIHunterStorage::RH_SYSTEM_USER_ID])) {
+            header('HTTP/1.0 400 Bad Request - id is not int.', true, 400);
+            die();
+        }
+
+        ROIHunterStorage::getInstance()->setStorage($data);
+
+        header("HTTP/1.1 200 OK");
+        die();
+    } else if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+        $content = json_encode(ROIHunterStorage::getInstance()->getStorageWithoutTokens());
+        header("HTTP/1.1 200 OK");
+        header("Content-Type:application/json");
+        echo($content);
+        die();
+    } else {
+        header('HTTP/1.0 405 Method Not Allowed', true, 405);
         die();
     }
-
-    ROIHunterStorage::getInstance()->setStorage($data);
-
-    header("HTTP/1.1 200 OK");
-    die();
-} elseif ($_SERVER['REQUEST_METHOD'] == 'GET') {
-    $content = json_encode(ROIHunterStorage::getInstance()->getStorageWithoutTokens());
-    header("HTTP/1.1 200 OK");
-    header("Content-Type:application/json");
-    echo($content);
-    die();
-} else {
-    header('HTTP/1.0 405 Method Not Allowed', true, 405);
+} catch (Exception $e) {
+    header("HTTP/1.1 500 Internal Server Error", true, 500);
+    echo("Unknown server exception: " . $e->getMessage() . " \nStacktrace: " . $e->getTraceAsString());
     die();
 }
